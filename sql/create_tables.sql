dbb1884527fb64da136aed75e4da1b443fda2a43
Andrew Pietruschka <andrew.pietruschka@snowflake.com>
2024-03-21 09:28:58 +1030

    Updates
    Updates

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,3 +1,3 @@
 /* Classification Setup Scripts */
-create or replace table patient_details(name varchar, email varchar, postcode number, medicare_nbr varchar, birth_year varchar, marital_status varchar, gender varchar, country varchar);
-create or replace table patient_details_2(name varchar, email varchar, postcode number, medicare_nbr varchar, birth_year varchar, marital_status varchar, gender varchar, country varchar);
+create or replace table patient_details(name varchar, email varchar, postcode number, medicare_number number, birth_year varchar, marital_status varchar, gender varchar, country varchar);
+create or replace table patient_details_2(name varchar, email varchar, postcode number, medicare_number number, birth_year varchar, marital_status varchar, gender varchar, country varchar);
