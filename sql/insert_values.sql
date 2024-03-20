0c424e800b657febfcb35cb067f776eab29788d2
Andrew Pietruschka <andrew.pietruschka@snowflake.com>
2024-03-21 09:41:43 +1030

    Updates

--- sql/insert_values.sql
+++ sql/insert_values.sql
@@ -1,14 +1,14 @@
-insert into patient_2("Scott Enterprise","scott@test.com","5000","4123123451","1999","single","male","Australia");
-insert into patient_2("Bev Jones","bev@abc.com",3000,"5123123222","1956","married","female","Australia");
-insert into patient_2("Lily James","lily@hotmail.com","2000","2123123454","1988","single","female","Australia");
-insert into patient_2("Dennis Lily","james@gmail.com","5011","3123123321","2001","married",",male","Australia");
+insert into patient_2('Scott Enterprise','scott@test.com',5000,4123123451,'1999','single','male','Australia');
+insert into patient_2('Bev Jones','bev@abc.com',3000,5123123222,'1956','married','female','Australia');
+insert into patient_2('Lily James','lily@hotmail.com',2000,2123123454,'1988','single','female','Australia');
+insert into patient_2('Dennis Lily','james@gmail.com',5011,3123123321,'2001','married',',male','Australia');
 
-insert into patient_2("Scott Enterprise","scott@test.com","5000","4123123451","1999","single","male","Australia");
-insert into patient_2("Bev Jones","bev@abc.com",3000,"5123123222","1956","married","female","Australia");
-insert into patient_2("Lily James","lily@hotmail.com","2000","2123123454","1988","single","female","Australia");
-insert into patient_2("Dennis Lily","james@gmail.com","5011","3123123321","2001","married",",male","Australia");
+insert into patient_2('Scott Enterprise','scott@test.com',5000,4123123451,'1999','single','male','Australia');
+insert into patient_2('Bev Jones','bev@abc.com',3000,5123123222,'1956','married','female','Australia');
+insert into patient_2('Lily James','lily@hotmail.com',2000,2123123454,'1988','single','female','Australia');
+insert into patient_2('Dennis Lily','james@gmail.com',5011,3123123321,'2001','married',',male','Australia');
 
-insert into patient_2("Scott Enterprise","scott@test.com","5000","4123123451","1999","single","male","Australia");
-insert into patient_2("Bev Jones","bev@abc.com",3000,"5123123222","1956","married","female","Australia");
-insert into patient_2("Lily James","lily@hotmail.com","2000","2123123454","1988","single","female","Australia");
-insert into patient_2("Dennis Lily","james@gmail.com","5011","3123123321","2001","married",",male","Australia");
+insert into patient_2('Scott Enterprise','scott@test.com',5000,4123123451,'1999','single','male','Australia');
+insert into patient_2('Bev Jones','bev@abc.com',3000,5123123222,'1956','married','female','Australia');
+insert into patient_2('Lily James','lily@hotmail.com',2000,2123123454,'1988','single','female','Australia');
+insert into patient_2('Dennis Lily','james@gmail.com',5011,3123123321,'2001','married',',male','Australia');
